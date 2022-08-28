acc9d86193122890376411ad5b711934514ecec1
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2022-08-28 08:51:08 -0500

    Adaptador: Alternativo para Firabase

--- app/src/main/java/com/example/snapshots/HomeFragment.kt
+++ app/src/main/java/com/example/snapshots/HomeFragment.kt
@@ -6,16 +6,47 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.snapshots.databinding.FragmentHomeBinding
 import com.example.snapshots.databinding.ItemSnapshotBinding
+import com.firebase.ui.database.FirebaseListOptions
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.FirebaseDatabase
 
 class HomeFragment : Fragment() {
+
+    private lateinit var mBinding: FragmentHomeBinding
+    private lateinit var mFirebaseAdapter: FirebaseRecyclerAdapter<Snapshot, SnapshotHolderView>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        mBinding = FragmentHomeBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val query = FirebaseDatabase.getInstance().reference.child("snapshots")
+
+        val options = FirebaseRecyclerOptions.Builder<Snapshot>()
+            .setQuery(query, Snapshot::class.java).build()
+
+        mFirebaseAdapter = object : FirebaseRecyclerAdapter<Snapshot, SnapshotHolderView>(options){
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SnapshotHolderView {
+
+            }
+
+            override fun onBindViewHolder(
+                holder: SnapshotHolderView,
+                position: Int,
+                model: Snapshot
+            ) {
+                
+            }
+        }
+
+        super.onViewCreated(view, savedInstanceState)
     }
 
     inner class SnapshotHolderView(view: View) : RecyclerView.ViewHolder(view){
