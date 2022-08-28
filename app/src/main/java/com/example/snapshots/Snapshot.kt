4d85d097c2d3d4619457861d044e95ba45ccdeb9
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2022-08-28 10:52:29 -0500

    Diseño vista para agregrar Parte I

--- app/src/main/java/com/example/snapshots/Snapshot.kt
+++ app/src/main/java/com/example/snapshots/Snapshot.kt
@@ -7,4 +7,4 @@ data class Snapshot(
     var id: String = "",
     var title: String = "",
     var photoUrl: String = "",
-    var likeList: Map<String, Boolean>)
+    var likeList: Map<String, Boolean> = mutableMapOf())
